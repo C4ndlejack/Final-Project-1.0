16891ecbd85e133a92a59fe66a54f90a1a49d1b2
Candlejack <garouspeak@gmail.com>
2022-09-18 11:26:37 +0300

    main task is done

--- main task.cs
+++ main task.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+
+ Console.WriteLine("Введите строки: ");
+string[] stringArray1 = new string[10];
+for(int i = 0; i < stringArray1.Length; i++)
+{
+    stringArray1[i] = Console.ReadLine();
+}
+string[] stringArray2 = new string [stringArray1.Length];
+
+void NewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+        Console.WriteLine();
+}
+
+NewArray(stringArray1, stringArray2);
+PrintArray(stringArray2);
